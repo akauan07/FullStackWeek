fa3836a511afbf3b1297af9a3082fdf73478b0c5
Mauricio Akauan <71728120+akauan07@users.noreply.github.com>
2024-05-04 17:36:42 -0300

    feat: add footer compenent

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Search from "./(home)/_components/search";
 import BookingItem from "./_components/booking-item";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./(home)/_components/barbershop-item";
+import Footer from "./_components/footer";
 
 export default async function Home() {
 
@@ -33,6 +34,16 @@ export default async function Home() {
       </div>
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {
+            barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+        </div>
+      </div>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {
